98796200dee93166a5469230d3154b5c927df820
MisaelMa <amisael.amir.misael@gmail.com>
2020-04-25 12:47:34 -0500

    first commit

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,17 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+import VueCompositionApi from '@vue/composition-api';
 
 Vue.config.productionTip = false
-
+Vue.use(VueCompositionApi);
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    vuetify,
+    router,
+    store,
+    setup: () => {
+        return {}
+    },
+    render: h => h(App)
+}).$mount('#app');
