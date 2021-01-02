d0eb00b9d7a63e2e629356314bf4d31571ef743a
MisaelMa <amisael.amir.misael@gmail.com>
2021-01-02 14:05:27 -0500

    test

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import store from './store'
 import vuetify from './plugins/vuetify';
 import VueCompositionApi from '@vue/composition-api';
 
+
 Vue.config.productionTip = false
 Vue.use(VueCompositionApi);
 new Vue({
